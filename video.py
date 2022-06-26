c28f9f7af246a3adf3994526b9dbe314693ab9db
Bipin <87369440+bipinkrish@users.noreply.github.com>
2022-06-26 10:55:26 +0530

    Update video.py

--- video.py
+++ video.py
@@ -25,8 +25,7 @@ def compress(message):
     vfile = app.download_media(message, progress=progress)
     name = vfile.split("/")[-1]
     print(name)
-    #cmd = f'./ffmpeg/ffmpeg -i {vfile} -c:v libx265 -vtag hvc1 output-{message.id}.mkv'
-    cmd = f'./ffmpeg/ffmpeg -i {vfile} -c copy output-{message.id}.mov'
+    cmd = f'./ffmpeg/ffmpeg -i {vfile} -c:v libx265 -vtag hvc1 output-{message.id}.mkv'
     app.send_message(message.chat.id,"Compressing") 
     try:
         os.system(cmd)
